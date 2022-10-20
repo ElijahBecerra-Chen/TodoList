29537074523fa40723115ebd66dc0879c6823a43
Elijah <99235605+ElijahBecerra-Chen@users.noreply.github.com>
2022-10-20 16:42:08 +0800

    2022-10-20
    first commit

---
+++ app.js
@@ -0,0 +1,47 @@
+const todoList = document.querySelector(".todo-list")
+const taskInput = document.querySelector("#taskInput")
+const addBtn = document.querySelector("#addBtn")
+const list = document.querySelector(".todo-list")
+
+resetInput()
+
+// event delegation
+todoList.addEventListener("click", (e) => {
+  if (e.target.nodeName === "BUTTON") {
+    const btn = e.target
+    btn.parentElement.remove()
+  }
+})
+
+addBtn.addEventListener("click", () => {
+  addTask(taskInput.value)
+})
+
+taskInput.addEventListener("keydown", (e) => {
+  if (e.code === "Enter") {
+    addTask(taskInput.value)
+  }
+})
+
+function addTask(taskContent = "") {
+  taskContent = taskContent.trim()
+
+  if (taskContent !== "") {
+    createTaskElement(taskContent)
+    resetInput()
+  }
+}
+
+function createTaskElement(task) {
+  const taskElement = `<li class="todo-item">
+        <span class="item">${task}</span>
+        <button class="closeBtn">X</button>
+      </li>`
+
+  list.insertAdjacentHTML("afterbegin", taskElement)
+}
+
+function resetInput() {
+  taskInput.value = ""
+  taskInput.focus()
+}
